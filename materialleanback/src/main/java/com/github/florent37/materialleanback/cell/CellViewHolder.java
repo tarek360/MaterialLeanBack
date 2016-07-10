e79ea7b38ec7aa3143b625de6943aae8bbd8fd07
Ahmed Tarek <ahmed.tarek@code95.com>
2016-07-10 17:21:28 +0200

    remove cardview

--- materialleanback/src/main/java/com/github/florent37/materialleanback/cell/CellViewHolder.java
+++ materialleanback/src/main/java/com/github/florent37/materialleanback/cell/CellViewHolder.java
@@ -1,17 +1,14 @@
 package com.github.florent37.materialleanback.cell;
 
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
+import android.widget.FrameLayout;
 import com.github.florent37.materialleanback.MaterialLeanBack;
 import com.github.florent37.materialleanback.MaterialLeanBackSettings;
 import com.github.florent37.materialleanback.R;
 import com.nineoldandroids.animation.Animator;
-import com.nineoldandroids.animation.AnimatorListenerAdapter;
 import com.nineoldandroids.animation.AnimatorSet;
 import com.nineoldandroids.animation.ObjectAnimator;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,120 +17,96 @@ import java.util.List;
  */
 public class CellViewHolder extends RecyclerView.ViewHolder {
 
-    final static float scaleEnlarged = 1.2f;
-    final static float scaleReduced = 1.0f;
+  final static float scaleEnlarged = 1.2f;
+  final static float scaleReduced = 1.0f;
 
-    protected CardView cardView;
-    protected boolean enlarged = false;
+  protected FrameLayout cell;
+  protected boolean enlarged = false;
 
-    protected final MaterialLeanBack.Adapter adapter;
-    protected final MaterialLeanBack.ViewHolder viewHolder;
-    protected final MaterialLeanBackSettings settings;
-    public final int row;
+  protected final MaterialLeanBack.Adapter adapter;
+  protected final MaterialLeanBack.ViewHolder viewHolder;
+  protected final MaterialLeanBackSettings settings;
+  public final int row;
 
-    Animator currentAnimator;
+  Animator currentAnimator;
 
-    public CellViewHolder(View itemView, int row, MaterialLeanBack.Adapter adapter, MaterialLeanBackSettings settings) {
-        super(itemView);
-        this.row = row;
-        this.adapter = adapter;
-        this.settings = settings;
+  public CellViewHolder(View itemView, int row, MaterialLeanBack.Adapter adapter,
+      MaterialLeanBackSettings settings) {
+    super(itemView);
+    this.row = row;
+    this.adapter = adapter;
+    this.settings = settings;
 
-        cardView = (CardView) itemView.findViewById(R.id.cardView);
-        this.viewHolder = adapter.onCreateViewHolder(cardView,row);
-        this.viewHolder.row = row;
-        cardView.addView(viewHolder.itemView);
+    cell = (FrameLayout) itemView.findViewById(R.id.cell);
+    this.viewHolder = adapter.onCreateViewHolder(cell, row);
+    this.viewHolder.row = row;
+    cell.addView(viewHolder.itemView);
+  }
+
+  public void enlarge(boolean withAnimation) {
+    if (!enlarged && settings.animateCards) {
+
+      if (currentAnimator != null) {
+        currentAnimator.cancel();
+        currentAnimator = null;
+      }
+
+      int duration = withAnimation ? 300 : 0;
+
+      AnimatorSet animatorSet = new AnimatorSet();
+      animatorSet.setDuration(duration);
+
+      List<Animator> animatorList = new ArrayList<>();
+      animatorList.add(ObjectAnimator.ofFloat(cell, "scaleX", scaleEnlarged));
+      animatorList.add(ObjectAnimator.ofFloat(cell, "scaleY", scaleEnlarged));
+
+      animatorSet.playTogether(animatorList);
+      currentAnimator = animatorSet;
+      animatorSet.start();
+
+      enlarged = true;
     }
+  }
 
-    public void enlarge(boolean withAnimation) {
-        if (!enlarged && settings.animateCards) {
+  public void reduce(boolean withAnimation) {
+    if (enlarged && settings.animateCards) {
+      if (currentAnimator != null) {
+        currentAnimator.cancel();
+        currentAnimator = null;
+      }
 
-            if(currentAnimator != null) {
-                currentAnimator.cancel();
-                currentAnimator = null;
-            }
+      int duration = withAnimation ? 300 : 0;
 
-            int duration = withAnimation? 300 : 0;
+      AnimatorSet animatorSet = new AnimatorSet();
+      animatorSet.setDuration(duration);
 
-            AnimatorSet animatorSet = new AnimatorSet();
-            animatorSet.setDuration(duration);
+      List<Animator> animatorList = new ArrayList<>();
+      animatorList.add(ObjectAnimator.ofFloat(cell, "scaleX", scaleReduced));
+      animatorList.add(ObjectAnimator.ofFloat(cell, "scaleY", scaleReduced));
 
-            List<Animator> animatorList = new ArrayList<>();
-            animatorList.add(ObjectAnimator.ofFloat(cardView, "scaleX", scaleEnlarged));
-            animatorList.add(ObjectAnimator.ofFloat(cardView, "scaleY", scaleEnlarged));
+      animatorSet.playTogether(animatorList);
+      currentAnimator = animatorSet;
+      animatorSet.start();
 
-            if(settings.overlapCards) {
-                //animatorList.add(ObjectAnimator.ofFloat(cardView, "translationX", translationX));
-                animatorSet.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-                        cardView.setCardElevation(settings.elevationEnlarged);
-                        currentAnimator = null;
-                    }
-                });
-            }
-
-            animatorSet.playTogether(animatorList);
-            currentAnimator = animatorSet;
-            animatorSet.start();
-
-            enlarged = true;
-        }
+      enlarged = false;
     }
+  }
 
-    public void reduce(boolean withAnimation) {
-        if (enlarged && settings.animateCards) {
-            if(currentAnimator != null) {
-                currentAnimator.cancel();
-                currentAnimator = null;
-            }
-
-            int duration = withAnimation? 300 : 0;
-
-            AnimatorSet animatorSet = new AnimatorSet();
-            animatorSet.setDuration(duration);
-
-            List<Animator> animatorList = new ArrayList<>();
-            animatorList.add(ObjectAnimator.ofFloat(cardView,"scaleX",scaleReduced));
-            animatorList.add(ObjectAnimator.ofFloat(cardView,"scaleY",scaleReduced));
-
-            if(settings.overlapCards) {
-                //animatorList.add(ObjectAnimator.ofFloat(cardView, "translationX", translationX));
-                animatorSet.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-                        cardView.setCardElevation(settings.elevationReduced);
-                        currentAnimator = null;
-                    }
-                });
-            }
-
-            animatorSet.playTogether(animatorList);
-            currentAnimator = animatorSet;
-            animatorSet.start();
-
-            enlarged = false;
-        }
+  public void newPosition(int position) {
+    if (position == 1) {
+      enlarge(true);
+    } else {
+      reduce(true);
     }
+  }
 
-    public void newPosition(int position) {
-        if (position == 1)
-            enlarge(true);
-        else
-            reduce(true);
-    }
+  public void onBind() {
+    int cell = getAdapterPosition() - CellAdapter.PLACEHOLDER_START_SIZE;
+    viewHolder.cell = cell;
+    adapter.onBindViewHolder(viewHolder, cell);
+  }
 
-    public void onBind() {
-        int cell = getAdapterPosition()-CellAdapter.PLACEHOLDER_START_SIZE;
-        viewHolder.cell = cell;
-        adapter.onBindViewHolder(viewHolder,cell);
-    }
-
-    public boolean isEnlarged() {
-        return enlarged;
-    }
-
-    public void setEnlarged(boolean enlarged) {
-        this.enlarged = enlarged;
-    }
+  public void setEnlarged(boolean enlarged) {
+    this.enlarged = enlarged;
+  }
 }
